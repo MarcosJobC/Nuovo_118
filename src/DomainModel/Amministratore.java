af6cf8d1e8e203cce4e6c43f17ddee33ccc547c6
Marco <marco.seraglini@stud.unifi.it>
2024-03-15 08:59:00 +0100

    Aggiornamento DM Amministratore

--- src/DomainModel/Amministratore.java
+++ src/DomainModel/Amministratore.java
@@ -1,23 +1,28 @@
 package DomainModel;
 
-//TODO AGGIUNGI ATTRIBUTI CORRETTI
-
 //TODO IL TEST CONTROLLER è AGGIUNGERE UTENTE E CONTROLLARE CHE SIA AGGIUNTO CORRETTAMENTE NEL DAO
+
 public class Amministratore {
 
     private int matricola;
     private String nome;
     private String cognome;
+    private String datadinascita; // Nota: mancava un punto e virgola qui
+    private String qualifica; // Nota: c'era un punto e virgola extra qui
     private String codiceFiscale;
-    private String qualifica;
-    // Altri campi e metodi
+    private String password;
+    private Boolean isAdmin; // Nota: Potrebbe essere meglio utilizzare un tipo booleano per isAdmin
 
-    public Amministratore(int matricola, String nome, String cognome, String codiceFiscale, String qualifica) {
+    // Costruttore
+    public Amministratore(int matricola, String nome, String cognome, String datadinascita, String qualifica, String codiceFiscale, String password, Boolean isAdmin) {
         this.matricola = matricola;
         this.nome = nome;
         this.cognome = cognome;
-        this.codiceFiscale = codiceFiscale;
+        this.datadinascita = datadinascita;
         this.qualifica = qualifica;
+        this.codiceFiscale = codiceFiscale;
+        this.password = password;
+        this.isAdmin = false;
     }
 
     // Metodi getter
@@ -33,12 +38,24 @@ public class Amministratore {
         return cognome;
     }
 
-    public String getCodiceFiscale() {
-        return codiceFiscale;
+    public String getDatadinascita() {
+        return datadinascita;
     }
 
     public String getQualifica() {
         return qualifica;
+    }
+
+    public String getCodiceFiscale() {
+        return codiceFiscale;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Boolean getIsAdmin() {
+        return isAdmin;
     }
 
     // Metodi setter
@@ -54,13 +71,24 @@ public class Amministratore {
         this.cognome = cognome;
     }
 
-    public void setCodiceFiscale(String codiceFiscale) {
-        this.codiceFiscale = codiceFiscale;
+    public void setDatadinascita(String datadinascita) {
+        this.datadinascita = datadinascita;
     }
 
     public void setQualifica(String qualifica) {
         this.qualifica = qualifica;
     }
 
+    public void setCodiceFiscale(String codiceFiscale) {
+        this.codiceFiscale = codiceFiscale;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setIsAdmin(Boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
 
 }
